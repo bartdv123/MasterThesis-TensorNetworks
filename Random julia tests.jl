a52d31b1526a53ab84169302fbbbe7780023987d
bartdv <bartdv123@gmail.com>
2024-01-11 11:45:18 +0100

    implementation of Tensor Rank-one approximations for creating tree networks

--- Random julia tests.jl
+++ Random julia tests.jl
@@ -1,3 +1,10 @@
-using LinearAlgebra
+using Tenet
 
-display(rand(3,3))
+# Create a vector
+my_vector = [1.0, 2.0, 3.0]
+
+# Convert the vector to a tensor with a named index
+my_tensor = Tensor(my_vector, (:i,))
+
+# Display the tensor
+@show my_tensor
